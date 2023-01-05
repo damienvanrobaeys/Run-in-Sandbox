893abbd7a9658f092841e07c2a96b91ceb0d9c87
Søren Elmholt <soelm@itrelation.dk>
2023-01-05 11:22:04 +0100

    Create Run_PSADT_In_PsExec.ps1

---
+++ Sources/Run_in_Sandbox/Run_PSADT_In_PsExec.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    [Parameter(Mandatory = $False)]
+    [string]$WorkDir
+) 
+
+if (!($WorkDirk)){
+$WorkDir = Read-Host "Enter the path for workdir"
+}
+
+$PSexec = "c:\pstools\PSexec.exe"
+$ServiceUI = "ServiceUI.exe -Process:explorer.exe Deploy-Application.exe -DeploymentType Install -DeployMode Interactive"
+
+$command = "$workdir\$ServiceUI"
+
+$cmd = "$psexec -w `"$workdir`" -si -accepteula $command"
+
+& { Invoke-Expression $cmd}

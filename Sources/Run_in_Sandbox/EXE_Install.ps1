68a4ee94d3b4293bc6484329919177ac05be32ac
Joly0 <julian0906@hotmail.de>
2023-03-10 17:11:09 +0100

    Fixed some typo´s Fixed exe-install

--- Sources/Run_in_Sandbox/EXE_Install.ps1
+++ Sources/Run_in_Sandbox/EXE_Install.ps1
@@ -1,9 +1,8 @@
-$Sandbox_Folder = "C:\Users\WDAGUtilityAccount\Desktop\Run_in_Sandbox"
+$Sandbox_Folder = "C:\Run_in_Sandbox"
+$Exe_Content_File = "$Sandbox_Folder\EXE_Command_File.txt"
+$ExeCommand = Get-Content $Exe_Content_File
+$Full_Exe_Path = $ExeCommand -Split "(?<=.exe)\s" | Select-Object -First 1
+$Directory_Path = (Get-Item $Full_Exe_Path).DirectoryName
 
-$FileName = (Get-Item $ScriptPath).BaseName
-
-New-Item "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs" -Force -Type Directory
-
-Set-Location "$Intunewin_Extracted_Folder\$FileName"
-$file = "$Sandbox_Folder\EXE_Command_File.txt"
-& { Invoke-Expression (Get-Content -Raw $file) }
+Set-Location "$Directory_Path"
+& { Invoke-Expression $ExeCommand }

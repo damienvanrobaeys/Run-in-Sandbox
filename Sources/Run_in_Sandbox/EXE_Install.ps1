0063f618e3a337dfe43e755f9eb5e232cc11175c
Joly0 <julian0906@hotmail.de>
2024-05-13 21:53:42 +0200

    Update EXE_Install.ps1

--- Sources/Run_in_Sandbox/EXE_Install.ps1
+++ Sources/Run_in_Sandbox/EXE_Install.ps1
@@ -1,10 +1,14 @@
 $Sandbox_Folder = "C:\Run_in_Sandbox"
-$FolderPath = Split-Path (Split-Path "$ScriptPath" -Parent) -Leaf
-$DirectoryName = (get-item $ScriptPath).DirectoryName
-$FileName = (get-item $ScriptPath).BaseName
-
-New-item "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs" -Force -Type Directory
-
-Set-Location "$Intunewin_Extracted_Folder\$FileName"
 $File = "$Sandbox_Folder\EXE_Command_File.txt"
-& { Invoke-Expression (Get-Content -Raw $File)}
+
+$Content = Get-Content -Raw $File
+$BaseFolder = Split-Path($Content.Split('"')[1])
+$Executable = Split-Path($Content.Split('"')[1]) -Leaf
+$Executable = "./$Executable"
+$Arguments = ($Content.Split('"',3)[-1]).Trim()
+
+$Command = $Executable + " " + $Arguments
+
+Set-Location -Path $BaseFolder
+
+& { Invoke-Expression $Command}
